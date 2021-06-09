bc52f428a569f66c4f4ffa07025d55e17d22a636
busFred <32878707+busFred@users.noreply.github.com>
2021-06-09 19:06:06 -0400

    add console output pdf

--- src/python/demo01.py
+++ src/python/demo01.py
@@ -1,14 +1,15 @@
-#%%
+# %%
 # valid function definition without type hint
 def print_hung_tien_in_map(input):
     print(input["Hung-Tien Huang"])
 
-#%%
+
+# %%
 # valid function call
 input = dict({"Hung-Tien Huang": "279-FR", "Chien-Ping, Wu": "KKA-0580"})
 print_hung_tien_in_map(input)
 
-#%%
+# %%
 # generate type error
 input = list(["Hung-Tien Huang", "Chien-Ping, Wu"])
 print_hung_tien_in_map(input)
